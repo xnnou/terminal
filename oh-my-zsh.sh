5f0c1807040e7a6088cd1901414e47a4bdbda9c4
xxnnou <54994424+xxnnou@users.noreply.github.com>
2020-05-28 15:16:33 +0700

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,1 +1,2 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
