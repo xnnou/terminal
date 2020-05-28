feb951d631b9787d4d854145ee175797addd69cc
xxnnou <54994424+xxnnou@users.noreply.github.com>
2020-05-28 15:20:59 +0700

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,2 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
