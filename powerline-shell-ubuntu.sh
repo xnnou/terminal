437963d1a78b1a0268177fce751438d4cfc3d42c
xxnnou <54994424+xxnnou@users.noreply.github.com>
2020-11-26 10:23:05 +0700

    Update powerline-shell-ubuntu.sh

--- powerline-shell-ubuntu.sh
+++ powerline-shell-ubuntu.sh
@@ -1,41 +1,43 @@
 # Date: 11-Aug-2020
 # Update: 25-Nov-2020
-# X-Powerline for bash vim tmux fish
+# Powerline for bash vim tmux fish
+#
+# https://github.com/b-ryan/powerline-shell
+# https://github.com/powerline/powerline
 # https://devpro.media/install-powerline-ubuntu/#
 # https://powerline.readthedocs.io/en/master/usage/shell-prompts.html
-# We only tested on Ubuntu 20.04
-sudo apt install tmux fish htop net-tools -y
+#
+#
+sudo apt install vim tmux fish htop net-tools -y
 sudo apt install python 
-
-pip install powerline-shell
-# https://github.com/b-ryan/powerline-shell.git
-git clone https://github.com/b-ryan/powerline-shell.git
-cd powerline-shell
+git clone https://github.com/powerline/powerline.git
+cd powerline 
 sudo python3 setup.py install
-
+#
+git clone https://github.com/powerline/fonts.git
+cd fonts
+sudo /install.sh
+#
+echo "================== BASH ====================================="
 echo 'function _update_ps1() {
     PS1=$(powerline-shell $?)
 }
 if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi' > ~.bashrc
-
+#
+echo "================== FISH ====================================="
 echo 'fish
 function fish_prompt
     powerline-shell --shell bare $status
 end ' > ~/.config/fish/config.fish
-
-#--- VIM ---#
-echo '--- VIM ---'
-sudo apt update -y
-sudo apt install -y vim
-
+#
+echo "================== VIM ======================================"
 echo 'python3 from powerline.vim import setup as powerline_setup
 python3 powerline_setup()
 python3 del powerline_setup
 set laststatus=2' > ~/.vimrc
-
-#--- tmux ---#
-echo '--- tmux ---'
+#
+echo "================== TMUX ====================================="
 echo 'set -g default-terminal "screen-256color"
 source "/usr/share/powerline/bindings/tmux/powerline.conf"' > ~/.tmux.conf
